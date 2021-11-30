934699e6a7ec6ca505aa5529e80fdafd491e0a8a
yuvischwartz <yuval.orderdesk@gmail.com>
2021-11-30 17:56:40 -0200

    I created the box model with tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <> </>
-  );
-}
+    <>
+      <div className={'grid grid-cols-12'}>
+        <div className={'col-span-12 border p-8 bg-gray-200'}>
+          <h1 className={'text-center text-2xl'}>App Title</h1>
+        </div>
 
-export default App;
+        <div className={'col-span-12 sm:col-span-4 border p-1'}>
+          <h1 className={'text-center'}>Categories</h1>
+        </div>
+
+        <div className={'col-span-12 sm:col-span-8 border p-1'}>
+          <h1 className={'text-center'}>Queistions</h1>
+        </div>
+
+      </div>
+  </>
+  );
+    }
+
+    export default App;
